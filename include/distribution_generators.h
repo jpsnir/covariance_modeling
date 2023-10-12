f5d6220bc9ae5a4948e328aa2bb83a4838cf512f
jagatpreet <nir.j@northeastern.edu>
2023-10-12 17:36:48 -0400

    added interface for distribution generation functions

---
+++ include/distribution_generators.h
@@ -0,0 +1,44 @@
+#ifndef DISTRIBUTION_GENERATORS_H
+#include <functional>
+#include <iostream>
+#include <random>
+
+/* @brief: interface class for generating different distributions
+ */
+namespace cvm {
+class DistributionGenerator {
+public:
+  DistributionGenerator() = default;
+
+  /* @brief Functor definition to be inherited by subclasses
+   * to create generator functions using inverse cdf principle
+   */
+  virtual std::function<double(std::mt19937 &)> operator()() = 0;
+};
+
+class StandardNormal : public DistributionGenerator {
+public:
+  StandardNormal() = default;
+
+  std::function<double(std::mt19937 &)> operator()() override {
+    auto generator = [](std::mt19937 &mt_gen) -> double {
+      std::normal_distribution<double> std_normal(0, 1);
+      return std_normal(mt_gen);
+    };
+    return generator;
+  };
+};
+class Exponential : public DistributionGenerator {
+public:
+  Exponential(double l) : lambda(l) {}
+  std::function<double(std::mt19937 &)> operator()() override {
+    auto generator = [this](std::mt19937 &mt_gen) -> double {
+      std::exponential_distribution<double> exp_dist(lambda);
+      return exp_dist(mt_gen);
+    };
+    return generator;
+  }
+  double lambda;
+};
+}; // namespace cvm
+#endif
